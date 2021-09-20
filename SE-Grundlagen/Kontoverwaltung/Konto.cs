f226b8a28b47e25a3c603e3ab1ad76f9e4188a32
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-09-20 14:32:49 +0200

    Kontostand

---
+++ SE-Grundlagen/Kontoverwaltung/Konto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontoverwaltung
+{
+    class Konto
+    {
+        //Felder
+        private decimal kontostand;
+
+        //Methoden
+        public void GeldEinzahlen(decimal betrag)
+        {
+            kontostand = kontostand + betrag;
+        }
+
+        public void GeldAuszahlen(decimal betrag)
+        {
+            kontostand -= betrag;
+        }
+
+        public decimal KontostandAnzeigen()
+        {
+            Console.WriteLine($"Kontostand: {kontostand:f2}");
+            return kontostand;
+        }
+    }
+}
---
+++ SE-Grundlagen/Kontoverwaltung/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Kontoverwaltung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto k1 = new Konto();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Kontoverwaltung:\n----------------");
+                Console.WriteLine("1) Geld einzahlen");
+                Console.WriteLine("2) Geld auszahlen");
+                Console.WriteLine("3) Kontostand abfragen");
+                Console.WriteLine("4) Beenden");
+                Console.Write("Bitte wählen: ");
+                string eingabe = Console.ReadLine();
+
+                if (eingabe == "1")
+                {
+                    //Geld einzahlen
+                    Console.WriteLine("Geldeinzahlen, welchen Betrag?");
+                    decimal betrag = Convert.ToDecimal(Console.ReadLine());
+                    k1.GeldEinzahlen(betrag);
+                }
+                else if (eingabe == "2")
+                {
+                    //Geld auszahlen
+                    Console.WriteLine("Geldauszahlen, welchen Betrag? ");
+                    decimal betrag = Convert.ToDecimal(Console.ReadLine());
+                    k1.GeldAuszahlen(betrag);
+                }
+                else if (eingabe == "3")
+                {
+                    //Kontostand anzeigen
+                    k1.KontostandAnzeigen();
+                    Console.WriteLine("Bitte Taste drücken ...");
+                    Console.ReadKey();
+                }
+                else if (eingabe == "4")
+                {
+                    return;
+                }else
+                {
+                    //Falsche Eingabe
+                    Console.WriteLine("Falsche Eingabe");
+                }
+            } while (true);
+            
+            //switch (eingabe)
+            //{
+            //    case "1":
+            //        //Geld einzahlen
+            //        break;
+            //    case "2":
+            //        //Geld auszahlen
+            //        break;
+            //    case "3":
+            //        //Kontostand anzeigen
+            //        break;
+            //    default:
+            //        //falsche Eingabe
+            //        break;
+            //}
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
