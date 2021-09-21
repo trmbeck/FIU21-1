7f88f6fecb540e1668dee064cdce592191ad2fbb
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-09-21 16:06:05 +0200

    Designer

---
+++ SE-Grundlagen/FormsDesigner/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace FormsDesigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.Eingabe = new System.Windows.Forms.TextBox();
+            this.btnAbbrechen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(339, 77);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Eingabe
+            // 
+            this.Eingabe.Location = new System.Drawing.Point(12, 36);
+            this.Eingabe.Name = "Eingabe";
+            this.Eingabe.Size = new System.Drawing.Size(402, 20);
+            this.Eingabe.TabIndex = 1;
+            this.Eingabe.MouseEnter += new System.EventHandler(this.Eingabe_MouseEnter);
+            this.Eingabe.MouseLeave += new System.EventHandler(this.Eingabe_MouseLeave);
+            // 
+            // btnAbbrechen
+            // 
+            this.btnAbbrechen.Location = new System.Drawing.Point(249, 77);
+            this.btnAbbrechen.Name = "btnAbbrechen";
+            this.btnAbbrechen.Size = new System.Drawing.Size(75, 23);
+            this.btnAbbrechen.TabIndex = 2;
+            this.btnAbbrechen.Text = "Abbrechen";
+            this.btnAbbrechen.UseVisualStyleBackColor = true;
+            this.btnAbbrechen.Click += new System.EventHandler(this.btnAbbrechen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 10);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bitte Text eingeben:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 77);
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(231, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(339, 7);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(426, 111);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAbbrechen);
+            this.Controls.Add(this.Eingabe);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Erstes Designer Fenster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox Eingabe;
+        private System.Windows.Forms.Button btnAbbrechen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ SE-Grundlagen/FormsDesigner/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsDesigner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Eingabe.Text = "OK";
+            if (progressBar1.Value == 9)
+                progressBar1.Value = 6;
+            progressBar1.Value += 1;
+        }
+
+        private void btnAbbrechen_Click(object sender, EventArgs e)
+        {
+            Eingabe.Text = "Abbrechen";
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+        }
+
+        private void Eingabe_MouseEnter(object sender, EventArgs e)
+        {
+            Eingabe.BackColor = Color.Yellow;
+        }
+
+        private void Eingabe_MouseLeave(object sender, EventArgs e)
+        {
+            Eingabe.BackColor = Color.Green;
+        }
+    }
+}
