4c2b5ac8aa22c46d33f359fa800522b8ac7055f5
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-09-21 12:13:06 +0200

    IPAdresse

---
+++ SE-Grundlagen/IPAdressenÜbung/IPAdresse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPAdressenÜbung
+{
+    class IPAdresse
+    {
+        //Felder
+        private string ipadresse;
+
+        //Konstruktoren (Überladung)
+        public IPAdresse()
+        {
+            this.ipadresse = "0.0.0.0";
+        }
+        public IPAdresse(int a, int b, int c, int d)
+        {
+            SetIP(a, b, c, d);
+        }
+        public IPAdresse(string ipadresse)
+        {
+            SetIP(ipadresse);
+        }
+
+        //Methoden
+        public bool SetIP(string ipadresse)
+        {
+            string[] oktetts = ipadresse.Split('.');
+            foreach (var item in oktetts)
+            {
+                byte oktett;
+                if (!byte.TryParse(item, out oktett))
+                {
+                    Console.WriteLine("Fehler: IPAdresse wird auf 0.0.0.0 gesetzt!!!");
+                    this.ipadresse = "0.0.0.0";
+                    return false;
+                };
+            }
+            this.ipadresse = ipadresse;
+            return true;
+        }
+        public bool SetIP(int a, int b, int c, int d)
+        {
+            if (a > -1 && a < 256 && b > -1 && b < 256 && c > -1 && c < 256 && d > -1 && d < 256)
+                ipadresse = a + "." + b + "." + c + "." + d;
+            else
+            {
+                ipadresse = "0.0.0.0";
+                Console.WriteLine("Fehler: IPAdresse wird auf 0.0.0.0 gesetzt!!!");
+                return false;
+            }
+            return true;
+        }
+        public string GetIP()
+        {
+            return ipadresse;
+        }
+
+        //Eigenschaften, Properties
+        public string IpAdresse
+        {
+            get
+            {
+                return GetIP();
+            }
+            set
+            {
+                SetIP(value);
+            }
+        }
+    }
+}
---
+++ SE-Grundlagen/IPAdressenÜbung/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IPAdressenÜbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAdresse ip1 = new IPAdresse("1033.1.1.1");
+            ip1.SetIP("1.2.3.4");
+            Debug.WriteLine(ip1.GetIP());
+
+            ip1.IpAdresse = "4.3.45.11";
+            Console.WriteLine(ip1.IpAdresse);
+
+        }
+    }
+}
