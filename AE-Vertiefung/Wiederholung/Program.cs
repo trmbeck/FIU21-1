f938ad8ce0c979e20b3dec95efeeda9642fc4b28
mbeck-tr <mbeck@lutz-und-grub.de>
2021-11-24 08:34:31 +0100

    AE-Vertiefung

---
+++ AE-Vertiefung/Wiederholung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wiederholung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
